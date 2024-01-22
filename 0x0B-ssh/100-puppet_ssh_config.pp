31c5e4ee4247cfa397645de0d335a4c49b27b962
Garrick Larson Potts <ricki.glp@gmail.com>
2024-01-22 11:44:05 -0800

    task adv corrections

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -7,7 +7,7 @@ file_line { 'Turn off passwd auth':
 }
 
 file_line { 'Declare identity file':
-  path  => '~/.ssh/config',
-  line  => 'IdentityFile ~/.ssh/school',
+  path  => '/root/.ssh/config',
+  line  => 'IdentityFile /root/.ssh/school',
   match => '^#?IdentityFile',
 }
