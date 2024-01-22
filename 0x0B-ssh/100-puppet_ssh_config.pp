72dcadeede556fd01b76ffdb857baaaab81104f4
Garrick Larson Potts <ricki.glp@gmail.com>
2024-01-22 11:56:43 -0800

    task adv corrections

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,13 +1,15 @@
 # Sets up SSH config file so that you can connect without typing a password
 
 file_line { 'Turn off passwd auth':
-  path  => '/etc/ssh/sshd_config',
-  line  => 'PasswordAuthentication no',
-  match => '^#?PasswordAuthentication',
+  ensure => present,
+  path   => '/root/.ssh/config',
+  line   => 'PasswordAuthentication no',
+  match  => '^#?PasswordAuthentication',
 }
 
 file_line { 'Declare identity file':
-  path  => '/root/.ssh/config',
-  line  => 'IdentityFile /root/.ssh/school',
-  match => '^#?IdentityFile',
+  ensure => present,
+  path   => '/root/.ssh/config',
+  line   => 'IdentityFile /root/.ssh/school',
+  match  => '^#?IdentityFile',
 }
